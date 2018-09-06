5028f54413ef009530fc30e66d5366a2325c742b
nsarvari <nsarvari@student.unimelb.edu.au>
2018-09-06 14:44:01 +1000

    db created

--- CarAuctionSystem/sql_DB_setup.sql
+++ CarAuctionSystem/sql_DB_setup.sql
@@ -1,28 +1,38 @@
-DROP TABLE buyer;
-DROP TABLE seller;
-DROP TABLE car;
-DROP TABLE buyer_car;
+CREATE SCHEMA APP;
 
-CREATE TABLE buyer(
-   id			INT GENERATED ALWAYS AS IDENTITY,
+DROP TABLE APP.buyer;
+DROP TABLE APP.seller;
+DROP TABLE APP.car;
+DROP TABLE APP.buyer_car;
+
+CREATE TABLE APP.buyer(
+   id			INT,
    username 	VARCHAR(50),
    password		VARCHAR(50),
    firstname	VARCHAR(50),
    lastname		VARCHAR(50),
-   phoneno		INT,
+   phoneno		VARCHAR(15),
    PRIMARY KEY (id)
 );
 
-CREATE TABLE seller(
-   id			INT GENERATED ALWAYS AS IDENTITY,
+INSERT INTO APP.buyer
+VALUES (1, 'buyer1', '1234', 'John', 'Wick', '0412345678');
+INSERT INTO APP.buyer
+VALUES (2, 'buyer2', '1234', 'Vito', 'Corleone', '0412345679');
+
+CREATE TABLE APP.seller(
+   id			INT,
    username 	VARCHAR(50),
    password		VARCHAR(50),
-   address		VARCHAR(50),
+   address		VARCHAR(100),
    PRIMARY KEY (id)
 );
 
-CREATE TABLE car(
-   id			INT GENERATED ALWAYS AS IDENTITY,
+INSERT INTO APP.seller
+VALUES (1, 'Seller', '1234', 'Parkville Victoria 3010');
+
+CREATE TABLE APP.car(
+   id			INT,
    sellerID		INT,
    regno	 	VARCHAR(50),
    make			VARCHAR(50),
@@ -32,12 +42,18 @@ CREATE TABLE car(
    salesdate	DATE,
    currentbid	FLOAT,
    PRIMARY KEY (id),
-   FOREIGN KEY (sellerID) REFERENCES seller (id)
+   FOREIGN KEY (sellerID) REFERENCES APP.seller (id)
 );
 
-CREATE TABLE buyer_car(
+INSERT INTO APP.car (id, sellerID, regno, make, model, year, price, salesdate)
+VALUES (1, 1, 'REG123', 'Toyota', 'Yaris', 2017, 9000, '06-09-18');
+
+INSERT INTO APP.car (id, sellerID, regno, make, model, year, price, salesdate)
+VALUES (2, 1, 'REG123', 'Mercedes', 'C-Class', 2014, 12000, '06-09-18');
+
+CREATE TABLE APP.buyer_car(
 	buyerID		INT,
 	carID		INT,
-	FOREIGN KEY (buyerID) REFERENCES buyer (id),
-	FOREIGN KEY (carID) REFERENCES car (id)
+	FOREIGN KEY (buyerID) REFERENCES APP.buyer (id),
+	FOREIGN KEY (carID) REFERENCES APP.car (id)
 );
