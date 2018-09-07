7ff545b7046698c74c4e274fb335f736666e967e
Yicong Li <liyicong.china@gmail.com>
2018-09-07 11:17:57 +1000

    update servlet name and create service layer class

--- CarAuctionSystem/src/car/auction/controller/ViewBuyersControllerServlet.java
+++ CarAuctionSystem/src/car/auction/controller/ViewBuyersControllerServlet.java
@@ -16,13 +16,13 @@ import javax.servlet.ServletContext;
  * Servlet implementation class UserInfoControllerServlet
  */
 @WebServlet("/buyers")
-public class BuyerInfoControllerServlet extends HttpServlet {
+public class ViewBuyersControllerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public BuyerInfoControllerServlet() {
+    public ViewBuyersControllerServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
---
+++ CarAuctionSystem/src/car/auction/domain/UserInfoManagementService.java
@@ -0,0 +1,12 @@
+package car.auction.domain;
+
+/*
+ * This class belong to service layer, mainly manage all the information of users
+ * */
+public class UserInfoManagementService {
+
+	public UserInfoManagementService() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
